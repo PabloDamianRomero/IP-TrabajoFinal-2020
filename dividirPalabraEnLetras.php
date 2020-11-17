1800765e094e3dc5d5fabcde45825a98d5d389f3
unknown <pablo.romero@est.fi.uncoma.edu.ar>
2020-11-17 10:59:06 -0300

    Función dividirPalabrasEnLetras corregida

--- dividirPalabraEnLetras.php
+++ dividirPalabraEnLetras.php
@@ -1,19 +1,21 @@
 <?php
-/** Punto 3 - Estructura C - 
+/** Punto 3 - Estructura C -
  * @return array
  */
-function dividirPalabraEnLetras($palabra){
-    $coleccionLetras = str_split ($palabra);
-    for ($i=0; $i < strlen ($palabra); $i++) { 
-        $coleccionLetras [$i] = array("letra"=> str_split($palabra)[$i], "descubierta" => "false");
-    }
-    print_r($coleccionLetras);
-   
+function dividirPalabraEnLetras($palabra)
+{
+ $coleccionLetras = str_split($palabra);
+ $longitudPalabra = strlen($palabra);
+ for ($i = 0; $i < $longitudPalabra; $i++) {
+  $coleccionLetras[$i] = array("letra" => str_split($palabra)[$i], "descubierta" => "false");
+ }
+ print_r($coleccionLetras);
+ return $coleccionLetras;
 }
 
+/*PROGRAMA PRINCIPAL*/
+echo "ingrese una palabra ";
+$palabra = trim(fgets(STDIN));
+$letras  = dividirPalabraEnLetras($palabra); //para probar la funcion
 
- /*PROGRAMA PRINCIPAL*/
- echo "ingrese una palabra ";
- $palabra = trim(fgets(STDIN));
- $letras = dividirPalabrasEnLetras ($palabra); //para probar la funcion
- 
+/** La función toma en cuenta los espacios de la palabra. Verificar si se permite */
